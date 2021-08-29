1ddabc390bb1a2c7b74ffd3c205dcd6e8e9298b9
arminazimi <arminazimi4030@gmail.com>
2021-08-30 01:46:36 +0430

    logging

---
+++ logging.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.Create("log.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+
+	f2, err := os.Open("no_file.txt")
+	if err != nil {
+		log.Println("error is", err)
+	}
+	defer f2.Close()
+
+}
