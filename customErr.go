bfb282cd52c1ae9cc0cca13243a18907c803c7f5
arminazimi <arminazimi4030@gmail.com>
2021-08-30 18:38:51 +0430

    edit

--- customErr.go
+++ customErr.go
@@ -1,10 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+)
 
 type customErr struct {
 	info string
 	id   int
+	err  error
 }
 
 func (e customErr) Error() string {
@@ -12,15 +17,31 @@ func (e customErr) Error() string {
 }
 
 func main() {
+	// ex1
 	c1 := customErr{
 		info: "I need more coffee",
 		id:   1,
 	}
 	foo(c1)
+
+	// ex2
+	_, err := bar(0)
+	if err != nil {
+		log.Println(err)
+	}
+
 }
 
 func foo(e error) {
 	fmt.Println("foo_", e, e.(customErr).id)
+}
+
+func bar(i int) (int, error) {
+	if i == 0 {
+		e := errors.New("more coffee needed")
+		return 0, customErr{"error", 1, e}
+	}
+	return i, nil
 }
 
 // that is why
